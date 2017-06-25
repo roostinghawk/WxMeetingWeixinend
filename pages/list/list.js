256206a25ddffdb6e55c350b882e8c946589dbc6
liuw <liuw198606@aliyun.com>
2017-06-25 19:22:11 +0800

    no message

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,3 +1,32 @@
 var app = getApp()
 Page({
+    data: {
+    motto: '会议记录',
+    meetings: {}
+  },
+  //事件处理函数
+  onLoad: function(options) {
+      var that = this;
+      if (!app.globalData.openid){
+        app.login();
+      }
+
+    // 获取会议一览
+    wx.request({
+        url: 'https://liuanchen.com/w/meetings/',
+        header: {
+            "token":app.globalData.token
+        },
+        success: function(res) {
+            that.setData({
+                meetings: res.data
+            })
+        },
+        fail: function(error) {
+        that.setData({
+                errMsg: error.errMsg
+            })
+        }
+    })
+  },
 });
